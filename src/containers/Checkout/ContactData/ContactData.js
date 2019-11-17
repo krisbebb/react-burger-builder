f67fb254f27becc508c0f09b6446be48d3b0ad9f
krisbebb <krisbebb@gmail.com>
2019-11-17 17:12:19 +1100

    added auth form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -104,25 +104,35 @@ class ContactData extends Component {
   };
 
   checkValidity(value, rules) {
-    let isValid = true
+    let isValid = true;
     if (!rules) {
-      return true
+        return true;
     }
-
+    
     if (rules.required) {
-      isValid = value.trim() !== '' && isValid
+        isValid = value.trim() !== '' && isValid;
     }
 
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid
+        isValid = value.length >= rules.minLength && isValid
     }
 
     if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid
+        isValid = value.length <= rules.maxLength && isValid
     }
 
-    return isValid
-  }
+    if (rules.isEmail) {
+        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        isValid = pattern.test(value) && isValid
+    }
+
+    if (rules.isNumeric) {
+        const pattern = /^\d+$/;
+        isValid = pattern.test(value) && isValid
+    }
+
+    return isValid;
+}
 
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedOrderForm = {
