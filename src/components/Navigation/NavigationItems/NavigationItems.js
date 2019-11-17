3c6ded356334c2665ebcbce2b0e2c2e34d7a2f67
krisbebb <krisbebb@gmail.com>
2019-11-17 22:01:13 +1100

    added signin

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -9,7 +9,7 @@ const navigationItems = () => {
         <ul className={classes.NavigationItems}>
             <NavigationItem link="/" exact >Burger Builder</NavigationItem>
             <NavigationItem link="/orders">Orders</NavigationItem>
-            <NavigationItem link="/auth">Log In</NavigationItem>
+            <NavigationItem link="/auth">Authenticate</NavigationItem>
         </ul>
         )
 }   
